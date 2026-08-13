e19384ed586e424cd579a89e9896b58e73fb4a3b
Ibrahim-Ulas <iboalpulas@hotmail.com>
2026-08-13 17:51:32 +0300

    mine

--- handler_upload_thumbnail.go
+++ handler_upload_thumbnail.go
@@ -1,12 +1,19 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"io"
 	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/bootdotdev/learn-file-storage-s3-golang-starter/internal/auth"
 	"github.com/google/uuid"
 )
+
+const maxMemory = 10
 
 func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Request) {
 	videoIDString := r.PathValue("videoID")
@@ -28,10 +35,51 @@ func (cfg *apiConfig) handlerUploadThumbnail(w http.ResponseWriter, r *http.Requ
 		return
 	}
 
-
 	fmt.Println("uploading thumbnail for video", videoID, "by user", userID)
+	err = r.ParseMultipartForm(maxMemory << 20)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error parsing form data", err)
+		return
+	}
+	file, fileHeader, err := r.FormFile("thumbnail")
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "File not found", err)
+		return
+	}
+	mediaType := fileHeader.Header.Get("Content-Type")
+	if !(mediaType == "image/jpeg" || mediaType == "image/png") {
+		respondWithError(w, http.StatusBadRequest, "Wrong media type", errors.New("mime type not allowed"))
+		return
+	}
+	fileExtension := strings.TrimPrefix(mediaType, "image/")
 
-	// TODO: implement the upload here
+	videoMetadata, err := cfg.db.GetVideo(videoID)
+	if err != nil {
+		respondWithError(w, http.StatusNotFound, "Video not found", err)
+		return
+	}
+	if videoMetadata.UserID != userID {
+		respondWithError(w, http.StatusUnauthorized, "You are not the owner of video", err)
+		return
+	}
 
-	respondWithJSON(w, http.StatusOK, struct{}{})
+	imagePath := videoIDString + "." + fileExtension
+	filePath := filepath.Join(cfg.assetsRoot, imagePath)
+	newFile, err := os.Create(filePath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error creating new file", err)
+		return
+	}
+	_, err = io.Copy(newFile, file)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error copying file", err)
+		return
+	}
+	joinedURL := fmt.Sprintf("http://localhost:%v", cfg.port+"/"+filePath)
+	videoMetadata.ThumbnailURL = &joinedURL
+	err = cfg.db.UpdateVideo(videoMetadata)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Error updating video", err)
+	}
+	respondWithJSON(w, http.StatusOK, videoMetadata)
 }
